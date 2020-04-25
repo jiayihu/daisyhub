62911235aa83feb92ef99c0efa417a9b47b4fde0
Jiayi Hu <jiayi.ghu@gmail.com>
2020-04-25 22:55:26 +0200

    Add bootstrap to client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Islands } from './features/islands/Islands';
+import { NotFound } from './features/404/NotFound';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/">
+          <Islands />
+        </Route>
+        <Route>
+          <NotFound />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ client/src/features/islands/Islands.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Button } from 'reactstrap';
+
+export function Islands() {
+  return (
+    <div>
+      <Button color="primary">Create an island</Button>
+    </div>
+  );
+}
