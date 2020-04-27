34a34c8a6a3df846b6153d44ae00aacf7633862f
Jiayi Hu <jiayi.ghu@gmail.com>
2020-04-27 23:31:44 +0200

    feat: add metrics service

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,9 @@ import express from 'express';
 import { cors, jsonError } from './middlewares';
 import { createBulletinsRoutes } from './routers/bulletins.router';
 import { db } from './db';
-import { createIslandsRoutes } from './routers/islands.router';
 import { createVisitorsRoutes } from './routers/visitors.router';
 import { createMessagesRoutes } from './routers/messages.router';
+import { createMetricsRoutes } from './routers/metrics.router';
 
 // Create an Express object and routes (in order)
 const app = express();
@@ -17,7 +17,7 @@ app.use(jsonError);
 app.use('/bulletins/:bulletinId/visitors', createVisitorsRoutes(db));
 app.use('/bulletins/:bulletinId/messages', createMessagesRoutes(db));
 app.use('/bulletins', createBulletinsRoutes(db));
-app.use('/islands', createIslandsRoutes());
+app.use('/metrics', createMetricsRoutes());
 
 exports.app = app;
 
---
+++ src/services/metrics.ts
@@ -0,0 +1,3 @@
+export function getMemoryStats() {
+  return process.memoryUsage();
+}
