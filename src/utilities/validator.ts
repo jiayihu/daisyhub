18c25e20922000e5f901c1f38bff3f6ddd9c553f
Jiayi Hu <jiayi.ghu@gmail.com>
2020-04-27 13:06:14 +0200

    fix(validator): use more standard error format

--- src/utilities/validator.ts
+++ src/utilities/validator.ts
@@ -2,6 +2,25 @@ import { RequestHandler } from 'express';
 import { Decoder } from 'io-ts/lib/Decoder';
 import { pipe } from 'fp-ts/lib/pipeable';
 import { fold } from 'fp-ts/lib/Either';
+import { Tree } from 'fp-ts/lib/Tree';
+import { flatten } from './utils';
+
+/**
+ * https://github.com/Microsoft/api-guidelines/blob/master/Guidelines.md#errorresponse--object
+ */
+export interface RestError {
+  code: 'BadArgument';
+  message: string;
+  details?: Array<RestError>;
+}
+
+function getErrorValues(forest: Array<Tree<string>>): Array<string> {
+  return flatten(
+    forest.map(x => {
+      return x.forest.length ? [x.value, ...getErrorValues(x.forest)] : [x.value];
+    }),
+  );
+}
 
 export const validator: <T>(decoder: Decoder<T>) => RequestHandler = decoder => (
   req,
@@ -11,7 +30,21 @@ export const validator: <T>(decoder: Decoder<T>) => RequestHandler = decoder => 
   return pipe(
     decoder.decode(req.body),
     fold(
-      errors => res.status(400).send({ status: 'error', error: errors }),
+      errorForest => {
+        const details: Array<RestError> = getErrorValues(errorForest).map(message => {
+          return {
+            code: 'BadArgument',
+            message,
+          };
+        });
+        const error: RestError = {
+          code: 'BadArgument',
+          message: 'Invalid request body',
+          details,
+        };
+
+        return res.status(400).send({ status: 'error', error });
+      },
       () => next(),
     ),
   );
