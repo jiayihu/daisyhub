92412bd7257d3ef0fd823b79ebe916b14e5d2a30
Jiayi Hu <jiayi.ghu@gmail.com>
2020-04-27 22:46:05 +0200

    feat: add messages service

--- src/routes/bulletins.ts
+++ src/routes/bulletins.ts
@@ -12,6 +12,8 @@ import { validator } from '../utilities/validator';
 import { BulletinBodyDec, PartialBulletinBodyDec, QueueBodyDec } from '../models/bulletin';
 import { getVisitors, addVisitor, removeVisitor, editVisitor } from '../services/visitors';
 import { VisitorBodyDec } from '../models/visitors';
+import { getMessages, addMessage, removeMessage } from '../services/messages';
+import { MessageBodyDec } from '../models/messages';
 
 export const createBulletinsRoutes = (db: Firestore) => {
   const router = express.Router();
@@ -89,6 +91,30 @@ export const createBulletinsRoutes = (db: Firestore) => {
     return removeVisitor(
       req.params.bulletinId,
       req.params.visitorId,
+    )(db)
+      .then(() => res.sendStatus(204))
+      .catch(next);
+  });
+
+  router.get('/:bulletinId/messages', (req, res, next) => {
+    return getMessages(req.params.bulletinId)(db)
+      .then(messages => res.send({ status: 'success', data: messages }))
+      .catch(next);
+  });
+
+  router.post('/:bulletinId/messages', validator(MessageBodyDec), (req, res, next) => {
+    return addMessage(
+      req.params.bulletinId,
+      req.body,
+    )(db)
+      .then(id => res.status(301).send({ status: 'success', data: { id } }))
+      .catch(next);
+  });
+
+  router.delete('/:bulletinId/messages/:messageId', (req, res, next) => {
+    return removeMessage(
+      req.params.bulletinId,
+      req.params.messageId,
     )(db)
       .then(() => res.sendStatus(204))
       .catch(next);
