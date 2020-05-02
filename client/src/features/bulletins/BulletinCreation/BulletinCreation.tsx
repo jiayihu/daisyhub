d88d1a697db9c3d916168cfb7a407f0f15935ae3
Federico Zanellato <zanellato.federico@gmail.com>
2020-05-02 16:15:04 +0200

    Add first Formik draft form

--- client/src/features/bulletins/BulletinCreation/BulletinCreation.tsx
+++ client/src/features/bulletins/BulletinCreation/BulletinCreation.tsx
@@ -1,7 +1,253 @@
 import React, { useState, useEffect } from 'react';
 import './BulletinCreation.scss';
-import { Container, Form, Col, FormGroup, Label, Input, Button, Collapse, Row } from 'reactstrap';
+import { Col, Collapse, FormGroup, Label, Spinner } from 'reactstrap';
 import { format } from 'date-fns';
+import { withFormik, FormikProps, Field, Form, FormikErrors } from 'formik';
+// import * as Yup from 'yup';
+
+type FormValues = {
+  dodoCode: string;
+  islandName: string;
+  playerName: string;
+  fruit: string;
+  date: string;
+  time: string;
+  price: number;
+  description: string;
+  fees: boolean;
+  concurrent: number;
+  queue: number;
+  isPrivate: boolean;
+};
+
+type OtherProps = {
+  toggle: (section: number) => void;
+  isOpen: { first: boolean; second: boolean; third: boolean };
+};
+
+const FirstForm = (props: OtherProps & FormikProps<FormValues>) => {
+  const { values, touched, errors, isSubmitting, handleChange, handleBlur, toggle, isOpen } = props;
+
+  return (
+    <Form>
+      <h2 onClick={() => toggle(1)}>About your Island</h2>
+      <Collapse isOpen={isOpen.first}>
+        <Col>
+          <FormGroup>
+            <Label for="dodo">Dodo Code</Label>
+            <Field type="dodoCode" name="dodoCode" />
+            {touched.dodoCode && errors.dodoCode}
+          </FormGroup>
+        </Col>
+        <Col>
+          <FormGroup>
+            <Label for="islandName">Island Name</Label>
+            <Field type="islandName" name="islandName" />
+            {touched.islandName && errors.islandName}
+          </FormGroup>
+        </Col>
+        <Col>
+          <FormGroup>
+            <Label for="playerName">Player Name</Label>
+            <Field type="playerName" name="playerName" />
+            {touched.playerName && errors.playerName}
+          </FormGroup>
+        </Col>
+        <Col>
+          <FormGroup>
+            <Label for="fruit">Fruit</Label>
+            <select name="fruit" value={values.fruit} onChange={handleChange} onBlur={handleBlur}>
+              <option value="" label="Choose your native fruit" />
+              <option value="apples" label="Apples" />
+              <option value="peaches" label="Peaches" />
+              <option value="oranges" label="Oranges" />
+              <option value="pears" label="Pears" />
+              <option value="cherries" label="Cherries" />
+            </select>
+            {touched.fruit && errors.fruit}
+          </FormGroup>
+        </Col>
+        <Col>
+          <FormGroup>
+            <Label for="date">Date</Label>
+            <input
+              name="date"
+              type="date"
+              onChange={date => {
+                console.log(date.target.value);
+                props.setFieldValue('date', date.target.value);
+              }}
+              onBlur={handleBlur}
+              value={values.date}
+            />
+          </FormGroup>
+        </Col>
+        <Col>
+          <FormGroup>
+            <Label for="time">Time</Label>
+            <input
+              name="time"
+              type="time"
+              onChange={time => {
+                props.setFieldValue('time', time.target.value);
+              }}
+              onBlur={handleBlur}
+              value={values.time}
+            />
+          </FormGroup>
+        </Col>
+      </Collapse>
+      <h2 onClick={() => toggle(2)}>About your turnips</h2>
+      <Collapse isOpen={isOpen.second}>
+        <Col>
+          <FormGroup>
+            <Label for="price">Selling price</Label>
+            <input
+              type="number"
+              name="price"
+              id="price"
+              onBlur={handleBlur}
+              onChange={handleChange}
+              value={values.price}
+              min={1}
+            />
+            {touched.price && errors.price}
+          </FormGroup>
+        </Col>
+        <Col>
+          <FormGroup>
+            <Label for="description">Description and requests</Label>
+            <input
+              type="textarea"
+              name="description"
+              id="description"
+              onChange={handleChange}
+              onBlur={handleBlur}
+            />
+          </FormGroup>
+        </Col>
+        <Col>
+          <FormGroup>
+            <Label for="fees">
+              <input
+                type="checkbox"
+                name="fees"
+                id="fees"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                checked={values.fees}
+              />
+              Entry fees
+            </Label>
+          </FormGroup>
+        </Col>
+      </Collapse>
+      <h2 onClick={() => toggle(3)}>About your queue</h2>
+
+      <Collapse isOpen={isOpen.third}>
+        <Col>
+          <FormGroup>
+            <Label for="concurrent">
+              How many people you want in your island (At the same time)
+            </Label>
+            <input
+              type="number"
+              name="concurrent"
+              id="concurrent"
+              onBlur={handleBlur}
+              onChange={handleChange}
+              value={values.concurrent}
+              min={1}
+            />
+          </FormGroup>
+        </Col>
+        <Col>
+          <FormGroup>
+            <Label for="queue">Max queue length</Label>
+            <input
+              type="number"
+              name="queue"
+              id="queue"
+              onBlur={handleBlur}
+              onChange={handleChange}
+              value={values.queue}
+              min={5}
+            />
+          </FormGroup>
+        </Col>
+        <Col>
+          <FormGroup>
+            <Label for="isPrivate">
+              <input
+                type="checkbox"
+                name="isPrivate"
+                id="isPrivate"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                checked={values.isPrivate}
+              />
+              Private island
+            </Label>
+          </FormGroup>
+        </Col>
+      </Collapse>
+
+      <button type="submit" disabled={isSubmitting}>
+        Submit
+      </button>
+    </Form>
+  );
+};
+
+type MyFormProps = {
+  initialDodo?: string;
+  myDate?: string;
+  myTime?: string;
+  toggle: (section: number) => void;
+  isOpen: { first: boolean; second: boolean; third: boolean };
+};
+
+const MyForm = withFormik<MyFormProps, FormValues>({
+  mapPropsToValues: props => {
+    return {
+      dodoCode: props.initialDodo || '',
+      islandName: '',
+      playerName: '',
+      fruit: '',
+      date: props.myDate || '',
+      time: props.myTime || '',
+      price: 100,
+      description: '',
+      fees: false,
+      concurrent: 4,
+      queue: 25,
+      isPrivate: false,
+    };
+  },
+
+  validate: (values: FormValues) => {
+    let errors: FormikErrors<FormValues> = {};
+    if (!values.dodoCode) {
+      errors.dodoCode = 'Required';
+    } else if (values.dodoCode.length !== 5) {
+      errors.dodoCode = 'Dodo Code must be exactly 5 characters';
+    }
+    if (!values.islandName) {
+      errors.islandName = 'Required';
+    }
+    if (!values.playerName) {
+      errors.playerName = 'Required';
+    }
+    if (!values.fruit) {
+      errors.fruit = 'Required';
+    }
+    return errors;
+  },
+
+  handleSubmit: values => {
+    return console.log(values);
+  },
+})(FirstForm);
 
 export const BulletinCreation = () => {
   const [isOpen, setIsOpen] = useState({
@@ -11,23 +257,22 @@ export const BulletinCreation = () => {
   });
 
   const [dateTime, setDateTime] = useState({ date: '', time: '' });
+  const [loading, setLoading] = useState(true);
 
   const initDateTime = () => {
     const today = new Date();
-    console.log('momo');
     const date = format(today, 'yyyy-MM-dd');
     const time = format(today, 'HH:mm');
     setDateTime({ date, time });
   };
 
   useEffect(() => {
-    if (isOpen.first === true) {
-      initDateTime();
-    }
-  }, [isOpen.first]);
+    initDateTime();
+    setLoading(false);
+  }, []);
 
-  const toggle = (n: number) => {
-    switch (n) {
+  const toggle = (section: number) => {
+    switch (section) {
       case 1:
         setIsOpen({ first: true, second: false, third: false });
         break;
@@ -42,105 +287,9 @@ export const BulletinCreation = () => {
     }
   };
 
-  return (
-    <Container style={{ backgroundColor: 'white' }}>
-      <Form>
-        <h2 onClick={() => toggle(1)}>About your Island</h2>
-        <Collapse isOpen={isOpen.first}>
-          <Col>
-            <FormGroup>
-              <Label for="dodo">Dodo Code</Label>
-              <Input type="text" name="dodoCode" id="dodoCode" placeholder="000000" />
-            </FormGroup>
-          </Col>
-          <Col>
-            <FormGroup>
-              <Label>Island Name</Label>
-              <Input type="text" name="name" id="islandName" placeholder="islandName" />
-            </FormGroup>
-          </Col>
-          <Col>
-            <FormGroup>
-              <Label for="player">Player Name</Label>
-              <Input type="text" name="player" id="playerName" placeholder="playerName" />
-            </FormGroup>
-          </Col>
-          <Col>
-            <FormGroup>
-              <Label for="fruit">Fruit</Label>
-              <Input type="select" name="fruit" id="fruit">
-                <option>Apples</option>
-                <option>Oranges</option>
-                <option>Cherries</option>
-                <option>Pears</option>
-                <option>Peaches</option>
-              </Input>
-            </FormGroup>
-          </Col>
-          <Label for="datetime">Date Time</Label>
-          <Row>
-            <Col>
-              <FormGroup>
-                <Input type="date" name="date" id="date" defaultValue={dateTime.date} />
-              </FormGroup>
-            </Col>
-            <Col>
-              <FormGroup>
-                <Input type="time" name="time" id="time" defaultValue={dateTime.time} />
-              </FormGroup>
-            </Col>
-          </Row>
-        </Collapse>
-        <h2 onClick={() => toggle(2)}>About the Turnips</h2>
-        <Collapse isOpen={isOpen.second}>
-          <Col>
-            <FormGroup>
-              <Label for="price">Selling price</Label>
-              <Input type="number" name="price" id="price" defaultValue="100" />
-            </FormGroup>
-          </Col>
-          <Col>
-            <FormGroup>
-              <Label for="description">Description and requests</Label>
-              <Input type="textarea" name="description" id="description" />
-            </FormGroup>
-          </Col>
-          <Col>
-            <FormGroup>
-              <Label for="fees">
-                <Input type="checkbox" />
-                Entry fees
-              </Label>
-            </FormGroup>
-          </Col>
-        </Collapse>
-        <h2 onClick={() => toggle(3)}>Queue management</h2>
-        <Collapse isOpen={isOpen.third}>
-          <Col>
-            <FormGroup>
-              <Label for="concurrent">
-                How many people you want in your island (At the same time)
-              </Label>
-              <Input type="number" name="concurrent" id="concurrent" defaultValue={5} />
-            </FormGroup>
-          </Col>
-          <Col>
-            <FormGroup>
-              <Label for="queue">Max queue length</Label>
-              <Input type="number" name="queue" id="queue" defaultValue={25} />
-            </FormGroup>
-          </Col>
-          <Col>
-            <FormGroup>
-              <Label for="isPrivate">
-                <Input type="checkbox" />
-                Private island
-              </Label>
-            </FormGroup>
-          </Col>
-        </Collapse>
-        <Button>Submit</Button>
-      </Form>
-    </Container>
+  return loading ? (
+    <Spinner type="grow" />
+  ) : (
+    <MyForm myDate={dateTime.date} myTime={dateTime.time} toggle={toggle} isOpen={isOpen} />
   );
 };
